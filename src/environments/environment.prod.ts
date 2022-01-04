503f0537bf7d0826ce409e1c8095b1ad7695ffd8
Nguyễn Văn Hùng <hungnv74@onemount.com>
2022-01-04 22:04:46 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,15 @@ export const environment = {
   production: true,
   version: env.npm_package_version,
   serverUrl: '/api',
-  envName: 'PROD'
+  envName: 'PROD',
+  host: 'https://fakestoreapi.com',
+  firebase: {
+    apiKey: 'AIzaSyBewBx3sI8UKfBE9Y1vCdMJ85F-fqDMswQ',
+    authDomain: 'angular-stock-1740f.firebaseapp.com',
+    projectId: 'angular-stock-1740f',
+    storageBucket: 'angular-stock-1740f.appspot.com',
+    messagingSenderId: '728871156809',
+    appId: '1:728871156809:web:148a31ba00dfef47125398',
+    measurementId: 'G-DG38P3DP4V'
+  }
 };
