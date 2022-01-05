c4859bf276ccf9f1b58e1665da86f53a93672e6a
Nguyễn Văn Hùng <hungnv74@onemount.com>
2022-01-05 21:39:24 +0700

    update

--- src/app/modules/tool/page/tool/tool.component.ts
+++ src/app/modules/tool/page/tool/tool.component.ts
@@ -20,6 +20,8 @@ export class ToolComponent extends DestroyComponent implements OnInit {
   symbols: any[] = [];
   active = 1;
   stocks: any[] = [];
+  volstocks: any[] = [];
+  threredstocks: any[] = [];
 
   constructor(
     private modalService: NgbModal,
@@ -59,6 +61,7 @@ export class ToolComponent extends DestroyComponent implements OnInit {
     );
   }
   onGetListRsi() {
+    this.stocks = [];
     this.symbols.forEach(symbol => {
       this.projectService.getHistorical(symbol.symbol, 21).subscribe(data => {
         if (data?.length && data[0].dealVolume > 356000) {
@@ -104,6 +107,137 @@ export class ToolComponent extends DestroyComponent implements OnInit {
             this.ref.markForCheck();
           }
           console.log(`${data[0].symbol}`, rsi);
+        }
+      });
+    });
+  }
+
+  getListVolUp() {
+    this.volstocks = [];
+    this.symbols.forEach(symbol => {
+      this.projectService.getHistorical(symbol.symbol, 26).subscribe(data => {
+        if (data?.length && data[0].dealVolume > 356000) {
+          data.forEach((d, index) => {
+            if (index < data.length - 1) {
+              d.change = (d.priceClose / d.adjRatio) - (data[index + 1].priceClose / data[index + 1].adjRatio);
+              if (d.change >= 0) {
+                d.up = d.change;
+                d.down = 0;
+              } else {
+                d.up = 0;
+                d.down = (d.change) * (-1);
+              }
+            }
+            return d;
+          });
+          let upt = 0;
+          let downt = 0;
+          let voltotal = 0;
+          let pricetotal = 0;
+          data.forEach((f, index) => {
+            voltotal += f.dealVolume;
+            pricetotal += (f.priceClose / f.adjRatio);
+            if (index < data.length - 1 &&  f.up >= 0) {
+              upt += f.up;
+            }
+            if (index < data.length - 1 &&  f.down >= 0) {
+              downt += f.down;
+            }
+          });
+          // console.log(`${data[0].symbol}`, data);
+
+          const avgu = upt/(data.length - 1);
+          const avgd = downt/(data.length - 1);
+          const avgvol = voltotal/(data.length);
+          const avgprice = pricetotal/(data.length);
+
+          let rs;
+          let rsi;
+          if (avgu && avgd) {
+            rs = avgu/avgd;
+            rsi = 100 - 100/(1 + rs);
+          } else {
+            rsi = 0;
+          }
+          data[0].rsi = rsi;
+          if (data[0].dealVolume > avgvol && data[0].change > 0.5 && data[1].change < 0.5) {
+            this.volstocks.push(data[0]);
+            this.ref.markForCheck();
+          }
+         
+
+          // if (data[0].change < 0 && data[1].change < 0 && data[2].change < 0) {
+          //   this.volstocks.push(data[0]);
+          //   this.ref.markForCheck();
+          // }
+         
+          // console.log(`${data[0].symbol}`, rsi);
+        }
+      });
+    });
+  }
+
+  
+  getListThrePrice() {
+    this.threredstocks = [];
+    this.symbols.forEach(symbol => {
+      this.projectService.getHistorical(symbol.symbol, 9).subscribe(data => {
+        if (data?.length && data[0].dealVolume > 356000) {
+          data.forEach((d, index) => {
+            if (index < data.length - 1) {
+              d.change = (d.priceClose / d.adjRatio) - (data[index + 1].priceClose / data[index + 1].adjRatio);
+              if (d.change >= 0) {
+                d.up = d.change;
+                d.down = 0;
+              } else {
+                d.up = 0;
+                d.down = (d.change) * (-1);
+              }
+            }
+            return d;
+          });
+          let upt = 0;
+          let downt = 0;
+          let voltotal = 0;
+          let pricetotal = 0;
+          data.forEach((f, index) => {
+            voltotal += f.dealVolume;
+            pricetotal += (f.priceClose / f.adjRatio);
+            if (index < data.length - 1 &&  f.up >= 0) {
+              upt += f.up;
+            }
+            if (index < data.length - 1 &&  f.down >= 0) {
+              downt += f.down;
+            }
+          });
+          // console.log(`${data[0].symbol}`, data);
+
+          const avgu = upt/(data.length - 1);
+          const avgd = downt/(data.length - 1);
+          const avgvol = voltotal/(data.length);
+          const avgprice = pricetotal/(data.length);
+
+          let rs;
+          let rsi;
+          if (avgu && avgd) {
+            rs = avgu/avgd;
+            rsi = 100 - 100/(1 + rs);
+          } else {
+            rsi = 0;
+          }
+          data[0].rsi = rsi;
+          // if (data[0].dealVolume > avgvol && data[0].change > 0.5 && data[1].change < 0.5) {
+          //   this.volstocks.push(data[0]);
+          //   this.ref.markForCheck();
+          // }
+         
+
+          if (data[0].change < 0 && data[1].change < 0 && data[2].change < 0) {
+            this.threredstocks.push(data[0]);
+            this.ref.markForCheck();
+          }
+         
+          // console.log(`${data[0].symbol}`, rsi);
         }
       });
     });
